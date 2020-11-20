890e15f359d6ce0e799cadea451dacb558c888e8
alex851209 <alex851007@gmail.com>
2020-11-20 14:25:12 +0800

    feat: Establish initial Publish page

--- Publisher/Publisher/HomeVC.swift
+++ Publisher/Publisher/HomeVC.swift
@@ -10,14 +10,19 @@ import UIKit
 class HomeVC: UIViewController {
 
     @IBOutlet weak var articleTableView: UITableView!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var publishView: UIView!
     
     let dataManager = DataManager()
+    var maskView = UIView()
     var articles = [Article]()
     var authors = [Author]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupPublishPage()
+        setupButton()
         setupTableView()
         fetchData()
     }
@@ -31,10 +36,41 @@ class HomeVC: UIViewController {
     func fetchData() {
         
         dataManager.listenArticle(completion: { [weak self] articles, authors in
-//            print("###\n\(authors.count)\n###")
             self?.articles = articles
             self?.authors = authors
             self?.articleTableView.reloadData()
+        })
+    }
+    
+    func setupButton() {
+        
+        addButton.layer.cornerRadius = 30
+        addButton.clipsToBounds = true
+        addButton.addTarget(self, action: #selector(addArticle), for: .touchUpInside)
+    }
+    
+    func setupPublishPage() {
+        
+        maskView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        maskView.backgroundColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.2)
+        maskView.isHidden = true
+        view.insertSubview(maskView, belowSubview: publishView)
+        
+        publishView.frame = CGRect(x: 16, y: view.frame.maxY, width: view.frame.width - 32, height: 500)
+        publishView.layer.cornerRadius = 10
+        publishView.clipsToBounds = true
+        publishView.isHidden = true
+    }
+    
+    @objc func addArticle() {
+        
+        maskView.isHidden = false
+        publishView.isHidden = false
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.5, delay: 0, animations: {
+            self.publishView.frame = CGRect(x: 16,
+                                            y: self.articleTableView.frame.minY + 130,
+                                            width: self.view.frame.width - 32,
+                                            height: 500)
         })
     }
 }
